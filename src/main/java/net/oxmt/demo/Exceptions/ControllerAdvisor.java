fd1c0b6aeaa6b7b0956afea566ebf63254c0820e
Matt <matt.spindler@oxmt.net>
2022-01-02 15:24:01 +0000

    Clean up

---
+++ src/main/java/net/oxmt/demo/Exceptions/ControllerAdvisor.java
@@ -0,0 +1,58 @@
+package net.oxmt.demo.Exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDate.now());
+        body.put("status", status.value());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("Errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, HttpServletRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDate.now());
+
+        try {
+            List<String> errors = ex.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+            body.put("Errors", errors);
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Unhandled error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
--- src/main/java/net/oxmt/demo/controllers/InventoryValidatorController.java
+++ src/main/java/net/oxmt/demo/controllers/InventoryValidatorController.java
@@ -1,24 +1,45 @@
 package net.oxmt.demo.controllers;
-
-
-import net.oxmt.demo.model.MachineAge;
+import net.oxmt.demo.model.InvalidAgeStringException;
+import net.oxmt.demo.model.InvalidAgeUnitsException;
 import net.oxmt.demo.model.MachineInformation;
+import net.oxmt.demo.model.dtos.MachineInformationDto;
+import net.oxmt.demo.service.EmptyMachineInformationException;
 import net.oxmt.demo.service.MachineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@Validated
 @RequestMapping("/inventory")
 public class InventoryValidatorController {
     @Autowired()
     MachineService _machineService;
 
     @PostMapping("/validate")
-    List<MachineInformation> validate(@RequestBody List<MachineInformation> machineInformation) {
-        return _machineService.validate(machineInformation);
-    }
+    List<MachineInformation> validate( @RequestBody @NotEmpty List<@Valid  MachineInformationDto> machineInformationDtoList) throws EmptyMachineInformationException, InvalidAgeUnitsException, InvalidAgeStringException {
 
+
+    List<MachineInformation> z = machineInformationDtoList.stream().map(x -> {
+        try {
+            return x.toMachineInformation();
+        } catch (InvalidAgeUnitsException e) {
+            e.printStackTrace();
+        } catch (InvalidAgeStringException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }).collect(Collectors.toList());
+
+
+
+        return _machineService.getOutliers(z);
+
+    }
 }
