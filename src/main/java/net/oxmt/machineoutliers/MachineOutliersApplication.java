4f98549a31dcecaa52d465750152bf83ed3935ad
Matt <matt.spindler@oxmt.net>
2022-01-02 15:50:17 +0000

    Clean up

--- src/main/java/net/oxmt/machineoutliers/MachineOutliersApplication.java
+++ src/main/java/net/oxmt/machineoutliers/MachineOutliersApplication.java
@@ -1,13 +1,13 @@
-package net.oxmt.demo;
+package net.oxmt.machineoutliers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MachineOutliersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MachineOutliersApplication.class, args);
     }
 
 }
